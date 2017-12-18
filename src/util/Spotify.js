48a3acf257c01dca00cf0880d82fb10d4d41c111
LaDean Clark <ladean.clark@salesforce.com>
2017-12-17 19:21:55 -0800

    Basic component structure setup

---
+++ src/util/Spotify.js
@@ -0,0 +1,18 @@
+import {credentials} from '../config/credentials';
+
+const accessToken = '';
+const tokenType = '';
+const expiresIn= 0;
+const refreshToken= '';
+
+export const Spotify = {
+    search(term) {
+        fetch() {
+
+        }
+    },
+
+    submit(playlist) {
+
+    }
+};
