74819efe9817907353dbb242d369f5c54ed5296d
LaDean Clark <ladean.clark@salesforce.com>
2017-12-19 15:53:18 -0800

    Can add to list and remove from list

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,8 +2,26 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleTrackAction = this.handleTrackAction.bind(this);
+    }
+
+    handleTrackAction(event) {
+        this.props.trackAction(this.props.track);
+        event.preventDefault();
+    }
+
     render() {
-        return <p>Track</p>;
+        return (
+          <div className="Track">
+            <div className="Track-information">
+              <h3>{this.props.track.name}</h3>
+              <p>{this.props.track.artist} | {this.props.track.album}</p>
+            </div>
+            <a className="Track-action" onClick={this.handleTrackAction}>{this.props.actionSymbol}</a>
+          </div>
+        );
     }
 }
 
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
+    //remove track
+        // parent function
+
+    //add track
+        //parent function
+
     render() {
-        return <p>TrackList</p>;
+        return (
+            <div className="TrackList">
+                {   
+                    this.props.trackList.map( track => {
+                      return <Track track={track} key={track.id} actionSymbol={this.props.actionSymbol} trackAction={this.props.trackAction} />;
+                    })
+                }   
+            </div>
+        );
     }
 }   
 
